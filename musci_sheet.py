05cd0c894776c59ab3cf4b21a950e0f6f1927694
MartinWho2 <martin.crettol@gmail.com>
2021-12-02 19:33:59 +0100

    Going on

--- musci_sheet.py
+++ musci_sheet.py
@@ -2,28 +2,6 @@ from xml.dom import minidom
 import csv
 LEN_FILES = 67
 root = minidom.Document()
-ronde = 100
-blanche = round(ronde/2)
-noire_pointée = round(ronde*3/8)
-noire = round(ronde/4)
-croche_pointée = round(ronde*3/16)
-croche = round(ronde/8)
-double_croche = round(ronde/16)
-notes = {ronde:"ronde",blanche:"blanche",noire_pointée:"noire_pointee",noire:"noire",croche_pointée:"croche_pointee",
-        croche:"croche",double_croche:"double_croche"}
-durations = [ronde,blanche,noire_pointée,noire,croche_pointée,croche,double_croche]
-
-def get_duration(note):
-    return notes[min(durations, key=lambda x: abs(x - note))]
-
-for i in range(LEN_FILES):
-    with open(str(i)+".csv","r") as file:
-        read = csv.reader(file, delimiter=";")
-        for row in read:
-            print(row)
-            for i in range(1,len(row)+1,2):
-                print(get_duration(int(row[i])),end=" ")
-
 
 xml = root.createElement('score-partwise')
 root.appendChild(xml)
